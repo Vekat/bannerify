a1318859bebf89aa0a899e0fec0720bc4499be31
Vitor Cortez <vitor.a.cortez@gmail.com>
2015-10-14 23:29:45 -0300

    Add option `pkg` to pass `package.json` explicitly

--- test/test.js
+++ test/test.js
@@ -34,11 +34,27 @@ describe('bannerify', function() {
   });
 
   it('should work with template file', function(done) {
-    var header = fs.readFileSync('./test/fixtures/banner.txt');
+    var bannerPath = './test/fixtures/banner.txt';
     var b = browserify();
     b.add('./test/fixtures/beep');
     b.plugin(bannerify, {
-      template: header
+      file: bannerPath
+    });
+    b.bundle(function(err, buff) {
+      assert.ifError(err);
+      assert.notEqual(buff.toString().indexOf('Mirco Zeiss <mirco.zeiss@gmail.com>'), -1);
+      done();
+    });
+  });
+
+  it('should work with explicit package', function(done) {
+    var header = fs.readFileSync('./test/fixtures/banner.txt');
+    var packagePath = './package.json';
+    var b = browserify();
+    b.add('./test/fixtures/beep');
+    b.plugin(bannerify, {
+      template: header,
+      pkg: packagePath
     });
     b.bundle(function(err, buff) {
       assert.ifError(err);
